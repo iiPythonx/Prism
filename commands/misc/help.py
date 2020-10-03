adfb136f653e8fa8026497041152064e450b23c3
Benjamin O'Brien <bobrien9274@gmail.com>
2020-10-03 15:47:19 -0500

    Fix and clean up the help command

--- commands/misc/help.py
+++ commands/misc/help.py
@@ -3,8 +3,6 @@ import discord
 import inspect
 
 import importlib
-from json import loads
-
 from os import listdir
 
 from assets.prism import Tools
@@ -15,10 +13,10 @@ class Help(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
-    self.desc = "Prism's help command, making life easy"
-    self.usage = "help [category]"
+    self.desc = "The help command, listing commands and providing details."
+    self.usage = "help [category/command]"
 
-  def get_commands(self, ctx, category):
+  def get_commands(self, category):
 
     commands = ""
 
@@ -30,127 +28,93 @@ class Help(commands.Cog):
 
     return f"``{commands[2:]}``"
 
-  async def embed(self, ctx, cat, prefix, commandClass = None):
-
-    embed = None
-
-    if cat == "default":
-
-      embed = discord.Embed(title = "The Prism Discord Bot", description = "The only discord bot you actually need.", url = "https://top.gg/bot/685550504276787200", color = 0x126bf1)
-
-      embed.add_field(name = ":person_golfing:  Entertainment Commands  :person_golfing:", value = f"See commands with ``{prefix}help entertain``", inline = False)
-
-      embed.add_field(name = ":cat:  Pet Commands  :dog:", value = f"See commands with ``{prefix}help pets``", inline = False)
-
-      embed.add_field(name = ":moneybag: Currency Commands :moneybag:", value = f"See commands with ``{prefix}help currency``", inline = False)
-
-      embed.add_field(name = ":tools:  Moderation Commands  :tools:", value = f"See commands with ``{prefix}help mod``", inline = False)
-
-      embed.add_field(name = ":newspaper:  Miscellaneous  Commands  :newspaper:", value = f"See commands with ``{prefix}help misc``", inline = False)
-
-    elif cat == "entertainment":
-
-      embed = discord.Embed(title = ":person_golfing:  Entertainment Commands  :person_golfing:", description = "The most interesting and unique commands out there.", color = 0x126bf1)
-
-      embed.add_field(name = "Commands", value = self.get_commands(ctx, cat), inline = False)
-
-    elif cat == "misc":
-
-      embed = discord.Embed(title = ":newspaper:  Miscellaneous  Commands  :newspaper:", description = "The commands that cannot be described.", color = 0x126bf1)
-
-      embed.add_field(name = "Commands", value = self.get_commands(ctx, cat), inline = False)
-
-    elif cat == "pets":
-
-      embed = discord.Embed(title = ":cat:  Pet Commands  :dog:", description = "Time to go walk the dog, (digitally of course lmao).", color = 0x126bf1)
-
-      embed.add_field(name = "Commands", value = self.get_commands(ctx, cat), inline = False)
-
-    elif cat == "currency":
-
-      embed = discord.Embed(title = ":moneybag: Currency Commands :moneybag:", description = "Go get a job you little lazy man.", color = 0x126bf1)
-    
-      embed.add_field(name = "Commands", value = self.get_commands(ctx, cat), inline = False)
-
-    elif cat == "moderation":
-
-      embed = discord.Embed(title = ":tools:  Moderation Commands  :tools:", description = "Somebody causing problems? Don't worry about it mate.", color = 0x126bf1)
-
-      embed.add_field(name = "Commands", value = self.get_commands(ctx, cat), inline = False)
-
-    else:
-
-      embed = discord.Embed(title = f"{cat} Command", description = f"{commandClass.desc}; usage: {commandClass.usage}", color = 0x126bf1)
-
-      embed.set_author(name = " | Help", icon_url = self.bot.user.avatar_url)
-
-      embed.set_footer(text = f" | Requested by {ctx.author}.", icon_url = ctx.author.avatar_url)
-      
-    return embed
-
   @commands.command(aliases = ["info", "commands", "cmds"])
   async def help(self, ctx, *, category = None):
 
-    if ctx.guild:
-
-      db = loads(open("db/guilds", "r").read())
-
-      prefix = db[str(ctx.guild.id)]["prefix"]
-
-    else:
-
-      prefix = "p!"
-
     if not category:
 
-      return await ctx.send(embed = await self.embed(ctx, "default", prefix))
+      embed = discord.Embed(title = "The Prism Discord Bot", description = "The only discord bot you actually need.", url = "https://top.gg/bot/685550504276787200", color = 0x126bf1)
+
+      embed.add_field(name = ":person_golfing:  Entertainment Commands  :person_golfing:", value = f"See commands with ``{ctx.prefix}help fun``", inline = False)
+
+      embed.add_field(name = ":cat:  Pet Commands  :dog:", value = f"See commands with ``{ctx.prefix}help pets``", inline = False)
+
+      embed.add_field(name = ":musical_note: Music Commands (beta) :musical_note:", value = f"See commands with ``{ctx.prefix}help music``", inline = False)
+
+      embed.add_field(name = ":moneybag: Currency Commands :moneybag:", value = f"See commands with ``{ctx.prefix}help currency``", inline = False)
+
+      embed.add_field(name = ":tools:  Moderation Commands  :tools:", value = f"See commands with ``{ctx.prefix}help mod``", inline = False)
+
+      embed.add_field(name = ":newspaper:  Miscellaneous  Commands  :newspaper:", value = f"See commands with ``{ctx.prefix}help misc``", inline = False)
 
     elif category.lower() in ["fun", "entertain", "entertainment"]:
         
-      return await ctx.send(embed = await self.embed(ctx, "entertainment", prefix))
+      embed = discord.Embed(title = ":person_golfing:  Entertainment Commands  :person_golfing:", description = "The most interesting and unique commands out there.", color = 0x126bf1)
+
+      embed.add_field(name = "Commands", value = self.get_commands("entertainment"), inline = False)
     
     elif category.lower() == "misc":
         
-      return await ctx.send(embed = await self.embed(ctx, "misc", prefix))
+      embed = discord.Embed(title = ":newspaper:  Miscellaneous  Commands  :newspaper:", description = "The commands that cannot be described.", color = 0x126bf1)
+
+      embed.add_field(name = "Commands", value = self.get_commands("misc"), inline = False)
       
     elif category.lower() == "pets":
         
-      return await ctx.send(embed = await self.embed(ctx, "pets", prefix))
-    
+      embed = discord.Embed(title = ":cat:  Pet Commands  :dog:", description = "Time to go walk the dog, (digitally of course lmao).", color = 0x126bf1)
+
+      embed.add_field(name = "Commands", value = self.get_commands("pets"), inline = False)
+
+    elif category.lower() == "music":
+        
+      embed = discord.Embed(title = ":musical_note:  Music Commands  :musical_note:", description = "Is it just me, or did low quality audio arrive?", color = 0x126bf1)
+
+      embed.add_field(name = "Commands", value = self.get_commands("music"), inline = False)
+
     elif category.lower() in ["currency", "economy"]:
         
-      return await ctx.send(embed = await self.embed(ctx, "currency", prefix))
+      embed = discord.Embed(title = ":moneybag: Currency Commands :moneybag:", description = "Go get a job you little lazy man.", color = 0x126bf1)
+    
+      embed.add_field(name = "Commands", value = self.get_commands("currency"), inline = False)
     
     elif category.lower() in ["mod", "moderation"]:
         
-      return await ctx.send(embed = await self.embed(ctx, "moderation", prefix))
+      embed = discord.Embed(title = ":tools:  Moderation Commands  :tools:", description = "Somebody causing problems? Don't worry about it mate.", color = 0x126bf1)
+
+      embed.add_field(name = "Commands", value = self.get_commands("moderation"), inline = False)
     
     else:
 
-      for command_folder in listdir("commands"):
+      command_path = None
 
-        for command in listdir(f"commands/{command_folder}"):
-            
-          if f"{category.lower()}.py" == command:
-            
-            command_module = importlib.import_module(f"commands.{command_folder}.{command[:-3]}")
+      for folder in listdir("commands"):
 
-            for name, obj in inspect.getmembers(command_module):
-                    
-              if inspect.isclass(obj):
-                        
-                try:
+        for file in listdir(f"commands/{folder}"):
 
-                  commandClass = obj(self.bot)
-      
-                except:
+          if file.endswith(".py") and file[:-3].lower() == category.lower():
 
-                  return await ctx.send(embed = Tools.error("That command is bugged (so stay tuned)."))
+            command_path = f"commands.{folder}.{file[:-3]}"
 
-                return await ctx.send(embed = await self.embed(ctx, Tools.uppercase(category), prefix, commandClass))
+      if not command_path:
 
-      return await ctx.send(embed = await self.embed(ctx, "default", prefix))
+        return await ctx.send(embed = Tools.error("That command/category could not be found."))
+
+      command = importlib.import_module(command_path)
+
+      for class_ in inspect.getmembers(command, inspect.isclass):
+
+        if class_[0].lower() == category.lower():
+
+          called = class_[1](self.bot)
+
+          embed = discord.Embed(title = class_[0] + " Command", description = f"{called.desc}\nUsage: {called.usage}", color = 0x126bf1)
+
+    embed.set_author(name = " | Help", icon_url = self.bot.user.avatar_url)
+
+    embed.set_footer(text = f" | Requested by {ctx.author}.", icon_url = ctx.author.avatar_url)
+
+    return await ctx.send(embed = embed)
           
 # Link to bot
 def setup(bot):
-    bot.add_cog(Help(bot))
+  bot.add_cog(Help(bot))
