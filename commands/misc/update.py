b06560f5de2e33fb9fb0c94655d5ccbca80781e9
Benjamin O'Brien <bobrien9274@gmail.com>
2020-12-01 21:22:29 -0600

    Patch up update and uptime

--- commands/misc/update.py
+++ commands/misc/update.py
@@ -21,35 +21,31 @@ class Update(commands.Cog):
         await ctx.send(embed = discord.Embed(title = "Fetching commits...", color = 0x126bf1), delete_after = 0)
         
         try:
+            r = get(self.url, timeout = 2).json()
 
-            r = get(self.url).json()
-
-            try:
-
-                await m.delete()
-
-            except:
-
-                pass
+            try: await m.delete()
+            except: pass
         
         except:
-
             return await ctx.send(embed = Tools.error("Sorry, something went wrong while fetching updates."))
 
-        commit = r[0]["commit"]
+        commit = r[0]
+        _commit = commit["commit"]
+        verification = f":white_check_mark:" if _commit["verification"]["verified"] else ":x:"
 
-        verification = f":white_check_mark:" if commit["verification"]["verified"] else ":x:"
-
-        embed = discord.Embed(title = commit["message"], url = "https://github.com/ii-Python/Prism/commit/" + commit["url"].split("/")[-1], color = 0x126bf1)
+        embed = discord.Embed(
+            title = "Latest update: " + _commit["message"][0].lower() + _commit["message"][1:],
+            description = "Click [here](https://github.com/ii-Python/Prism/commits) to view previous updates.",
+            url = "https://github.com/ii-Python/Prism/commit/" + _commit["url"].split("/")[-1],
+            color = 0x126bf1
+        )
         
-        embed.add_field(name = "Pushed by", value = r[0]["committer"]["login"], inline = False)
-
-        embed.add_field(name = "Push verified", value = verification, inline = False)
-
-        embed.add_field(name = "Pushed on", value = commit["committer"]["date"].split("T")[0], inline = False)
+        embed.add_field(name = "Pushed by", value = commit["author"]["login"])
+        embed.add_field(name = "Push verified", value = verification)
+        embed.add_field(name = "Pushed on", value = _commit["committer"]["date"].split("T")[0])
+        embed.add_field(name = "Push signature", value = commit["sha"][:-(len(commit["sha"]) - 7)])
 
         embed.set_author(name = " | Update", icon_url = self.bot.user.avatar_url)
-        
         embed.set_footer(text = f" | Requested by {ctx.author}.", icon_url = ctx.author.avatar_url)
         
         return await ctx.send(embed = embed)
