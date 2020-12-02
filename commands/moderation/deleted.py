0c3c3bc7cfeabbf7842f1c2b7fe1b74d7dc3c8eb
Benjamin O'Brien <bobrien9274@gmail.com>
2020-12-01 21:01:51 -0600

    Major overhaul to p!deleted

--- commands/moderation/deleted.py
+++ commands/moderation/deleted.py
@@ -10,7 +10,7 @@ class Deleted(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
-    self.desc = "Lists the last 5 deleted messages"
+    self.desc = "Lists the last 10 deleted messages"
     self.usage = "deleted"
 
   @commands.command(aliases = ["messages", "delmes", "deletedmessages"])
@@ -18,22 +18,17 @@ class Deleted(commands.Cog):
   async def deleted(self, ctx):
 
     db = json.loads(open("db/guilds", "r").read())
+    msgs = db[str(ctx.guild.id)]["data"]["deleted_messages"]
 
-    messages = ""
-
-    for message in db[str(ctx.guild.id)]["data"]["deleted_messages"]:
-
-      messages = f"{messages}{message}\n"
-
-    if not messages:
-
+    if not msgs:
       return await ctx.send(embed = Tools.error("This server doesn't have any deleted messages."))
 
-    embed = discord.Embed(description = messages, color = 0x126bf1)
-
+    embed = discord.Embed(title = "Last 10 deleted messages", description = "This does not list embeds or messages longer than 100 characters.", color = 0x126bf1)
     embed.set_author(name = " | Deleted Messages", icon_url = self.bot.user.avatar_url)
-
     embed.set_footer(text = f" | Requested by {ctx.author}.", icon_url = ctx.author.avatar_url)
+
+    for msg in msgs:
+      embed.add_field(name = f"{msg['author']} ({msg['date']}):", value = msg['content'], inline = False)
 
     return await ctx.send(embed = embed)
 
